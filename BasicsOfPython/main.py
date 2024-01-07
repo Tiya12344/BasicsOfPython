5f728c81a3345206cd41819c0504edaa9f97dd28
Tiya Noonia <tiyanoonia2901@gmail.com>
2024-01-07 18:26:39 +0000

    Here in this is multiple assignments

--- BasicsOfPython/main.py
+++ BasicsOfPython/main.py
@@ -1,6 +1,23 @@
-if 7 > 14 :
-    print ("7 is less than 14.")
-elif 6 > 7 :
-    print ("6 is greater than 7")
-else :
-    print ("Oops none of the conditions are true")
+# if 7 > 4 :
+#     print ("7 is less than 14.")
+# elif 6 > 7 :
+#     print ("6 is greater than 7")
+# else :
+#     print ("Oops none of the conditions are true")
+
+# name = "Tiya"
+# age = 10
+
+name,age = "Tiya",10
+print(f"name : {name} | age : {age}")
+
+x = y = z = "banana"
+print(f"x : {x}")
+print(f"y : {y}")
+print(f"z : {z}")
+
+fruits = ["Watermelon","Mango","Apples"]
+x , y ,z = fruits
+print(f"x : {x}")
+print(f"y : {y}")
+print(f"z : {z}")
